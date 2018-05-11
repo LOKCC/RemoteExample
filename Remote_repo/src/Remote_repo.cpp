20ea937caa3d35603cdb79c8254fe2f1f00db4fd
cheecong <cheecong@cheecong-MOBL.gar.corp.intel.com>
2018-05-11 15:51:42 +0800

    Remote initial commit

---
+++ Remote_repo/src/Remote_repo.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Remote_repo.cpp
+// Author      : lok
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello Github" << endl; // prints Hello refactor
+	return 0;
+}
